01fcf7cfbeeb1931e8e79968c9824ba6f55412b7
santhiya velu <santhiyav1507@gmail.com>
2025-06-04 21:13:46 +0100

    playercontainer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,28 @@ function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main id="game-container">
+      <div id="players">
+        <ol>
+          <li>
+            <span className="player">
+          <span className="players-name">player 1</span>
+          <span className="players-symbol">X</span>
+          <button>Edit</button>
+          </span>
+          </li>
+          <li>
+          <span className="player">
+          <span className="players-name">player 2</span>
+          <span className="players-symbol">X</span>
+          <button>Edit</button>
+          </span>
+          </li>
+        </ol>
+
+      </div>
+    
+    </main>
   )
 }
 
